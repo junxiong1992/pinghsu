e287254019b16b9bb87f313c60954ae3c7421dc8
夕日 <xirike@163.com>
2018-05-14 21:53:20 +0800

    404页面汉化

--- 404.php
+++ 404.php
@@ -9,7 +9,7 @@
 		<meta http-equiv="Cache-Control" content="no-siteapp"/><?php if($this->options->favicon): ?>
         <link rel="shortcut icon" href="<?php $this->options->favicon(); ?>"><?php endif;?><?php if($this->options->iosicon): ?>
         <link rel="apple-touch-icon" href="<?php $this->options->iosicon();?>"><?php endif; ?>
-        <title>Nothing here - <?php $this->options->title() ?></title>
+        <title>页面找不到 - <?php $this->options->title() ?></title>
         <style>
         body{
             margin:0;padding:0;
@@ -130,8 +130,8 @@
                 </header>
                 <section>
                     <div class="title404">
-                        <p>I have no secret of success but hard work.</p>
-                    </div><a href="<?php $this->options->siteUrl(); ?>" class="index404">Back Home</a>
+                        <p>哎呀！页面找不到了！去别处逛逛吧！</p>
+                    </div><a href="<?php $this->options->siteUrl(); ?>" class="index404">返回主页</a>
                 </section>
             </div>
         </div>
